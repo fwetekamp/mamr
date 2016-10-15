09bac4db8af7c1240cb85ef65b43bd1d5c2d78c3
fwetekamp <fabian.we@gmail.com>
2016-10-15 02:22:21 +0200

    Adding Address Selector with Mapkit
    Yet to test on physical device

---
+++ mamr WatchKit Extension/AddressInterfaceController.swift
@@ -0,0 +1,54 @@
+//
+//  AddressInterfaceController.swift
+//  mamr
+//
+//  Created by Fabian Wetekamp on 15/10/2016.
+//  Copyright © 2016 Fabian Wetekamp. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import CoreLocation
+
+
+
+class AddressInterfaceController: WKInterfaceController, CLLocationManagerDelegate {
+
+    @IBOutlet var mapObject: WKInterfaceMap!
+    var locationManager: CLLocationManager = CLLocationManager()
+    var mapLocation: CLLocationCoordinate2D?
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currentLocation = locations[0]
+        let lat = currentLocation.coordinate.latitude
+        let long = currentLocation.coordinate.longitude
+        self.mapLocation = CLLocationCoordinate2DMake(lat, long)
+        let span = MKCoordinateSpanMake(0.1, 0.1)
+        let region = MKCoordinateRegionMake(self.mapLocation!, span)
+        self.mapObject.setRegion(region)
+        self.mapObject.addAnnotation(self.mapLocation!, with: .red)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestLocation()
+
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
