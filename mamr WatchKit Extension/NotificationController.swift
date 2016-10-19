e1f43f40a98312732a8df19eaa2b1ec2cf1fa907
fwetekamp <fabian.we@gmail.com>
2016-10-20 01:12:17 +0200

    Code cleanup

--- mamr WatchKit Extension/NotificationController.swift
+++ mamr WatchKit Extension/NotificationController.swift
@@ -110,72 +110,12 @@ class NotificationController: WKUserNotificationInterfaceController {
             self.test_image.setImageNamed("Lunch_Day1_Dish1")
             self.test_image2.setImageNamed("Lunch_Day1_Dish2")
             }
-            
-        // This method is called when a notification needs to be presented.
-        // Implement it if you use a dynamic notification interface.
-        // Populate your dynamic notification interface as quickly as possible.
-        //
-        // After populating your dynamic notification interface call the completion block.
         completionHandler(.custom)
     
         }
     }
     
     
-
-    
-  /*  switch(response.actionIdentifier){
-    case "order1":
-        print("tapped order1")
-    case "order2":
-        print("tapped order2")
-    default: break
-    }*/
-
-    
-    
-    
-       /* let actionIdentifier = response.actionIdentifier
-        if actionIdentifier == "com.apple.UNNotificationDefaultActionIdentifier" || actionIdentifier == "com.apple.UNNotificationDismissActionIdentifier" {
-            return;
-        }
-        let dish1 = (actionIdentifier == "order1")
-        let dish2 = (actionIdentifier == "order2")
-    
-        repeat {
-            if (dish1) {
-                (print("order 1 pressed"))
-               break;
-            }
-            if (dish2) {
-                (print("order 2 pressed"))
-                break;
-            }
-        } while (false);
-       */ // Must be called when finished
-
-    
-    /*optional func didReceive(_ response: UNNotificationResponse,
-                             completionHandler completion: @escaping () -> Void)
-    {
-        
-        if response.actionIdentifier == "tomorrowReminder" {
-            print("Tomrrow Button Pressed")
-            subLabel.text = "Reminder For Tomorrow"
-            subLabel.textColor = UIColor.blue
-            done(.dismissAndForwardAction)
-        }
-        
-        if response.actionIdentifier == "dismissReminder" {
-            print("Dismiss Button Pressed")
-            done(.dismiss)
-            
-        } else {
-            print("Else response")
-            done(.dismissAndForwardAction)
-        }
-        
-    }*/
 
 }
 
